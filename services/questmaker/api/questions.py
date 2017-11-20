d1247a104c9b551e078218bbf9f9d0d1dffffc05
GrailFinder <wohilas@gmail.com>
2017-11-20 18:34:14 +0300

    make question

--- services/questmaker/api/questions.py
+++ services/questmaker/api/questions.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request, make_response, render_template
+from sqlalchemy import exc
 from services.questmaker.api.models import Question
 from services.questmaker import db
 
@@ -10,3 +11,40 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+@questions_blueprint.route('quest', methods=['POST'])
+def add_question():
+    # get data from request
+    post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
+
+    title = post_data.get('title')
+
+    try:
+        question = Question.query.filter_by(title=title).first()
+        if not question: # if there was no such question in db
+            db.session.add(Question(title=title))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{title} was added!'
+            }
+            return make_response(jsonify(response_object)), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'That question already exists.'
+            }
+            return make_response(jsonify(response_object)), 400
+    except (exc.IntegrityError, ValueError) as e:
+        db.session().rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
--- services/questmaker/tests/test_questions.py
+++ services/questmaker/tests/test_questions.py
@@ -1,5 +1,4 @@
 import json
-
 from services.questmaker.tests.base import BaseTestCase
 
 
@@ -13,3 +12,17 @@ class TestQuestionService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_add_question(self):
+        """Ensure that creating new question behaves normal"""
+        with self.client:
+            response = self.client.post(
+                "/quest",
+                data=json.dumps(dict(
+                    title="Can you read?"
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('success', data['status'])
