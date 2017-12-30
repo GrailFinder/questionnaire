c47d78580297f87d35b8e38092d4ec0e41662ca9
grail <wohilas@gmail.com>
2017-12-30 11:21:00 +0300

    input play

--- services/questmaker/api/inquiry.py
+++ services/questmaker/api/inquiry.py
@@ -1,62 +1,63 @@
 from flask import Blueprint, jsonify, request, make_response, render_template
 from sqlalchemy import exc
 from services.questmaker.api.models import Inquiry
+from services.questmaker.api.utils import authenticate
 from services.questmaker import db
 
 inquiry_blueprint = Blueprint('inquiry', __name__, template_folder='./templates')
 
 
-@inquiry_blueprint.route('/inquiries', methods=['POST', 'GET'])
-def add_inquiry():
+@inquiry_blueprint.route('/inquiries', methods=['POST'])
+@authenticate
+def add_inquiry(resp):
     
-    if request.method == 'POST':
-
-        # get data from request
-        post_data = request.get_json()
-        if not post_data:
-            response_object = {
-                'status': 'fail',
-                'message': 'Invalid payload.'
-            }
-            return make_response(jsonify(response_object)), 400
-
-        title = post_data.get('title')
-
-        try:
-            inquiry = Inquiry.query.filter_by(title=title).first()
-            if not inquiry: # if there was no such inquiry in db
-                db.session.add(Inquiry(title=title))
-                db.session.commit()
-                response_object = {
-                    'status': 'success',
-                    'message': f'{title} was added!'
-                }
-                return make_response(jsonify(response_object)), 201
-            else:
-                response_object = {
-                    'status': 'fail',
-                    'message': 'That inquiry already exists.'
-                }
-                return make_response(jsonify(response_object)), 400
-        except (exc.IntegrityError, ValueError) as e:
-            db.session().rollback()
-            response_object = {
-                'status': 'fail',
-                'message': 'Invalid payload.'
-            }
-            return make_response(jsonify(response_object)), 400
-
-    elif request.method == 'GET':
-        
-        inquiry = Inquiry.query.all()
-        data_list = [{'id': i.id, 'title': i.title, 'created_at': i.created_at,
-        'questions': [f'{q}' for q in i.questions]} for i in inquiry]
-
+    # get data from request
+    post_data = request.get_json()
+    if not post_data:
         response_object = {
-            'status': 'success',
-            'data': data_list,
-        }        
-        return jsonify(response_object), 200
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
+
+    title = post_data.get('title')
+
+    try:
+        inquiry = Inquiry.query.filter_by(title=resp).first()
+        if not inquiry: # if there was no such inquiry in db
+            db.session.add(Inquiry(title=title))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{title} was added!'
+            }
+            return make_response(jsonify(response_object)), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'That inquiry already exists.'
+            }
+            return make_response(jsonify(response_object)), 400
+    except (exc.IntegrityError, ValueError) as e:
+        db.session().rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
+
+@inquiry_blueprint.route('/inquiries', methods=['GET'])
+def get_inquiries():
+        
+    inquiry = Inquiry.query.all()
+    data_list = [{'id': i.id, 'title': i.title, 'created_at': i.created_at,
+    'questions': [f'{q}' for q in i.questions]} for i in inquiry]
+
+    response_object = {
+        'status': 'success',
+        'data': data_list,
+    }        
+    return jsonify(response_object), 200
 
 @inquiry_blueprint.route('/inquiry/<inquiry_id>', methods=['GET'])
 def get_single_inquiry(inquiry_id):
@@ -93,3 +94,8 @@ def show_index():
 def show_inquiry(inquiry_id):
     inquiry = Inquiry.query.filter_by(id=inquiry_id).first()
     return render_template('inquiry.html', questions=inquiry.questions)
+
+@inquiry_blueprint.route('/front-inq/<inquiry_id>', methods=['POST'])
+def send_inquiry(inquiry_id):
+    print(request.form)
+    return str(request.form)
--- services/questmaker/tests/test_inquery.py
+++ services/questmaker/tests/test_inquery.py
@@ -1,12 +1,23 @@
 import json
 from services.questmaker.tests.base import BaseTestCase
-from services.questmaker.tests.utils import add_quest, add_inquiry
+from services.questmaker.tests.utils import add_quest, add_inquiry, add_user
 
 class TestinquiryService(BaseTestCase):
     """Tests for the inquiries"""
 
     def test_add_inquiry(self):
         """Ensure that creating new inquiry behaves normal"""
+
+        user = add_user('test', 'test@test.com', 'test')
+        resp_login = self.client.post(
+            '/auth/login',
+            data=json.dumps(dict(
+                email='test@test.com',
+                password='test'
+            )),
+            content_type='application/json'
+        )
+
         with self.client:
             response = self.client.post(
                 "/inquiries",
@@ -14,6 +25,11 @@ class TestinquiryService(BaseTestCase):
                     title="testone"
                 )),
                 content_type='application/json',
+                headers=dict(
+                    Authorization='Bearer ' + json.loads(
+                        resp_login.data.decode()
+                    )['auth_token']
+                )
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 201)
