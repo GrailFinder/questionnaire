1f32f35f974e63f54fc3971412a1ce5a48edbffd
grail <wohilas@gmail.com>
2018-03-10 16:17:11 +0300

    Could not open a connection to your authentication agent.

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -7,4 +7,3 @@ openssl aes-256-cbc -K $encrypted_1f39af138ba1_key -iv $encrypted_1f39af138ba1_i
 rm id_rsa.enc # Don't need it anymore
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
