305753759283e8c5530c2f682f55d5a08dcfb338
grail <wohilas@gmail.com>
2018-03-10 13:34:47 +0300

    ssh-add

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -7,6 +7,7 @@ openssl aes-256-cbc -K $encrypted_1f39af138ba1_key -iv $encrypted_1f39af138ba1_i
 rm id_rsa.enc # Don't need it anymore
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 sudo rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname
   -s`-`uname -m` > docker-compose
