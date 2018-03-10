dc8ea426be61ccbe5d37ef7b0d5db13d8aae1a1d
grail <wohilas@gmail.com>
2018-03-10 14:05:01 +0300

    -in

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -3,7 +3,7 @@ set -x # Show the output of the following commands (useful for debugging)
     
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_1f39af138ba1_key -iv $encrypted_1f39af138ba1_iv in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_1f39af138ba1_key -iv $encrypted_1f39af138ba1_iv -in id_rsa.enc -out id_rsa -d
 rm id_rsa.enc # Don't need it anymore
 chmod 600 id_rsa
 mv id_rsa ~/.ssh/id_rsa
