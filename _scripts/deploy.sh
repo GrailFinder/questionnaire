c2f155e16cd304bb261484515c3dac50a2921e47
grail <wohilas@gmail.com>
2018-03-10 14:48:04 +0300

    what in dir

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -7,7 +7,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     git remote add deploy "deploy@159.65.124.54:/var/www/ankete"
     git config user.name "Travis CI"
     git config user.email "GrailFinder+travisCI@gmail.com"
-    
+    ls -la
     git add .
     git commit -m "Deploy"
     git push --force deploy master
